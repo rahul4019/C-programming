9e88ffbb4b68f727c599c9b8c2734c4a3bb8aec5
rahul4019 <rahulkashyap4019@gmail.com>
2024-08-10 20:05:51 +0530

    scanf function

---
+++ 09_input_function.c
@@ -0,0 +1,19 @@
+// Formatted Input Function in C 
+
+#include <stdio.h>
+
+void main() {
+    // int a, b, sum;
+    // scanf("%d %d", &a, &b);
+
+    // scanf("%4d %d", &a, &b);
+    // sum = a + b;
+    // printf("sum: %d", sum);
+
+    char ch;
+    scanf("%c", &ch);
+    printf("character\n: %c", ch);
+
+    return;
+
+}
