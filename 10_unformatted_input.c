1ee6851609f26aff5c1c423fb9568830efdb0582
rahul4019 <rahulkashyap4019@gmail.com>
2024-08-11 09:00:50 +0530

    Unformatted input function

---
+++ 10_unformatted_input.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    char ch;
+    // ch = getchar(); // reads single character 
+    
+    // printf("ch = %c\n",ch);
+
+    // char ch1 = getch(); // reads single character defined in conio.h headerfile
+    // doesn't wait for enter to be jumps to the next line
+    // and character doesn't get echoed on the screen
+    // used in sensetive cases like password
+
+    // printf("ch1 = %c\n", ch1);
+
+    // char ch2 = getche(); // reads single character
+    // doesn't wait for enter to be jumps to the next line
+    // and character gets echoed on the screen
+
+    // printf("ch2 = %c\n", ch2);
+
+    char str[20]; // character array of size 20
+    gets(str);
+    printf("Entered String: %s", str);
+
+    return 0;
+}
