b9527e6571f8470e1a990e5c7082220394261fc5
rahul4019 <rahulkashyap4019@gmail.com>
2024-08-15 07:24:35 +0530

    Different variant of for loops

---
+++ 17_loops.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    // for (int i = 0; i < 10; i++) {
+    //     printf("%d\n", i);
+    // }
+
+    // int i = 0;
+
+    // for (; i<= 5; i++) {
+    //     printf("%d\n", i);
+    // }
+
+    // int i;
+    // // this will not give any output but there will be no errors
+    // for ( i ; i <=5; i++) {
+    //     printf("%d\n",i);
+    // }
+
+    // for ( int i = 0, j = 1; i <= 5 && j <= 4; i++,j++) {
+    //     printf("i: %d\n",i);
+    //     printf("j: %d\n",j);
+    // }
+
+    //* infinite loop
+    // for (int i = 0, j = 1; ; i++) {
+    //     printf("%d%d\n",i,j);
+    // }
+
+    for (int i = 0; i == 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+
+
+}
