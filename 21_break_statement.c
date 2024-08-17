957656742ae07f1554ed5247bf446b835773c830
rahul4019 <rahulkashyap4019@gmail.com>
2024-08-17 19:30:46 +0530

    break statement

---
+++ 21_break_statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, i;
+    int sum = 0;
+
+    for(i = 0; i < 5; i++) {
+
+        printf("Enter number: \n");
+        scanf("%d", &a);
+
+        if ( a < 0) break;
+        sum += a;    
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
