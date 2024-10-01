effc31edce7c8c69ed668e2d74bfea514636da2b
rahul4019 <rahulkashyap4019@gmail.com>
2024-10-01 18:55:57 +0530

    Tricky pointer questions related associativity

--- 42_pointer_inc_dec.c
+++ 42_pointer_inc_dec.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int a[] = {3, 2, 67, 0, 56};
-  int *p = a;
+  /*int a[] = {3, 2, 67, 0, 56};*/
+  /*int *p = a;*/
 
   /*int i = 0;*/
   /*while (i < 5) {*/
@@ -27,8 +27,23 @@ int main() {
   /*  printf("a[%d] = %d\n", i, *p++);*/
   /*}*/
 
-  printf("%d %d\n", *(p++), *p++);
-  printf("%d\n", *p);
+  /*printf("%d %d\n", *(p++), *p++);*/
+  /*printf("%d\n", *p);*/
 
+  
+  int a [] = {10, 11, -1, 56, 67, 5, 4};
+  int *p, *q;
+  
+  p = a;
+  printf("%d\n", *p);
+  
+  /*printf("%d %d %d\n", (*p)++, *p++, *++p);*/
+  
+  /*printf("%d\n", (*p)++);*/
+
+  /*printf("%d\n", *p++);*/
+  /*printf("%d\n", *++p);*/
+
+  printf("%d %d %d\n", (*p)++, *p++, *++p);
   return 0;
 }
