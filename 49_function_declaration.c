0830b3d2c2ac1425acec7cc88c93b5c0d8e8bb3c
rahul4019 <rahulkashyap4019@gmail.com>
2024-10-03 16:07:37 +0530

    Function declaration

---
+++ 49_function_declaration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sum(int a, int b); // function declaration
+// sum(int a, int b); // it will give warning at compile time beacuse there is
+// no return type
+
+int main() {
+  int a, b;
+  printf("%s\n", "Enter two numbers:");
+  scanf_s("%d%d", &a, &b);
+
+  int result = sum(a, b);
+
+  printf("%s%d\n", "Sum = ", result);
+}
+
+int sum(int a, int b) { return a + b; } // function definition
